ddf613ec986bb92aafe279b2a5de0de9ad34c058
RajithaGayashan <rajithagayashan95@gmail.com>
2020-07-18 13:26:40 +0530

    DB connect java class updated

--- java_project/src/mycode/DBconnect.java
+++ java_project/src/mycode/DBconnect.java
@@ -1,7 +1,18 @@
 package mycode;
 import java.sql.Connection;
+import java.sql.DriverManager;
 public class DBconnect {
 public static Connection connect(){
+    Connection conn=null;
+    try{
+        
+        Class.forName("com.mysql.jdbc.Driver");
+        conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/my_java_project","root","");
+    }
+    catch(Exception e){
+        System.out.println(e);
+    }
     
+    return conn;
 }    
 }
